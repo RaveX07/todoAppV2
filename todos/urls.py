a6e69819090b13f4c8260dfd493380b9c243324b
RaveX07 <jannis.schroeter@gmx.de>
2023-06-04 13:57:30 +0200

    add login/logout

--- todos/urls.py
+++ todos/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
-    # path('login/', views.login_view, name='login'),
-    # path('logout/', views.logout_view, name='logout')
+    path('logout/', views.logout_view, name='logout')
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,18 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 
 # Create your views here.
 def home_view(request):
-    ctx = {
-        'user': request.user
-    }
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
 
-    return render(request, 'home.html', ctx)
+        user = authenticate(request, username=username, password=password)
 
-def login_view(request):
-    pass
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You've succesfully been succesfully logged in!")
+            return redirect('home')
+        else:
+            messages.success(request, "There was an error logging you in, please try again!")
+            return redirect('home')
+    else:
+        return render(request, 'home.html')
 
 def logout_view(request):
-    pass
+    logout(request)
+    messages.success(request, 'You have been logged out!')
+    return redirect('home')
