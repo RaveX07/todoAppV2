1fe2804f84f46f785b9b8e8965d335f14fe6cc63
RaveX07 <jannis.schroeter@gmx.de>
2023-06-05 11:06:55 +0200

    correct model

--- todos/models.py
+++ todos/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Todo(models.Model):
-    user = models.UUIDField()
+    user = models.CharField(max_length=35)
     content = models.CharField(max_length=50)
     dueTo = models.DateTimeField()
-    createdAt = models.DateTimeField()
+    createdAt = models.DateTimeField(auto_now_add=True)
     done = models.BooleanField(default=False)
     
