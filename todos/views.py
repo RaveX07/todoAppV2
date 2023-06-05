8ab3f2539bea5195ba97fc1c0b84de875703ed97
RaveX07 <jannis.schroeter@gmx.de>
2023-06-05 11:06:11 +0200

    create register view

--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import RegisterForm
 
 
 # Create your views here.
@@ -26,3 +27,20 @@ def logout_view(request):
     logout(request)
     messages.success(request, 'You have been logged out!')
     return redirect('home')
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You Have Successfully Registered! Welcome!")
+            return redirect('home')
+    else:
+        form = RegisterForm()
+        return render(request, 'register.html', {'form':form})
+
+    return render(request, 'register.html', {'form':form})
